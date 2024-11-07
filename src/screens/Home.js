cb89be01c50c1a4cd29be2362a078599d28820eb
CadeHuggins <78053936+CadeHuggins@users.noreply.github.com>
2024-11-07 01:06:54 +0000

    wddasdasd

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet, Dimensions, Alert } from 'react-native';
 import DriverList from '../features/DriverList';
+import { useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
 
 export default function Home() {
     return (
-        <DriverList/>
-    )
+        <View style={styles.container}>
+            <DriverList />
+            <Pressable style={styles.button}
+            onPress={() =>
+                navigate("/Emergencies")
+            }
+            >
+                <Text style={styles.buttonText}>Press Me</Text>
+            </Pressable>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Ensures the view takes up the full screen height
+  },
+  button: {
+    backgroundColor: '#841584',
+    alignItems: 'center',
+    width: Dimensions.get('window').width, // Full width of screen
+    padding: 15,
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 5, 
+  },
+  buttonText: {  // Corrected casing
+    color: '#fff',
+    fontSize: 16,
+  },
+});
